56d9cb8d4c71195c19e7a29cef63e747b726e639
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2019-03-18 16:51:06 +0100

    WIP universalviewer

--- components/record/UniversalViewer.vue
+++ components/record/UniversalViewer.vue
@@ -1,17 +1,21 @@
 <template>
-  <div id="uv">
+  <div ref="uv">
     universal viewer
   </div>
 </template>
 
 
 <script>
+  let uvPlayer;
   export default {
     mounted: function () {
-      console.log('mounted', typeof UV);
-      window.addEventListener('uvLoaded', () => {
-        console.log('uvLoaded');
-      }, false);
+      console.log('mounted', uvPlayer);
+      let urlDataProvider = new window.UV.URLDataProvider(true);
+      console.log(urlDataProvider);
+
+      uvPlayer = window.createUV(this.$refs['uv'], {
+        iiifResourceUri: 'https://iiif.europeana.eu/presentation/9200396/BibliographicResource_3000118435009/manifest'
+      });
     }
   };
 </script>
